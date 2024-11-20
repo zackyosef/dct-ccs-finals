23ec08370318472946748747a65efc9828e986db
Lucila Baluyut <lucibaluyutxiii@gmail.com>
2024-11-20 21:46:00 +0800

    added the css link to sync with dashboard

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -1,3 +1,6 @@
+<?php if (session_status() !== PHP_SESSION_ACTIVE) { session_start();}?>
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/functions.php');?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,8 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Add the custom-dashboard.css here -->
-    <title><!-- Insert Dynamic Title Here --></title>
+    <link rel="stylesheet" href="../../admin/partials/custom-dashboard.css">
+    <title>Student Management System</title>
 </head>
 
 <body>
