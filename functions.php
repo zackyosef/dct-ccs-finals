f62afb537fc0965d1dde5f0d5254ec9765e251e7
Lucila Baluyut <lucibaluyutxiii@gmail.com>
2024-11-20 19:45:00 +0800

    connection of database added

--- functions.php
+++ functions.php
@@ -1,3 +1,25 @@
 <?php    
     // All project functions should be placed here
+
+    // Database Configuration
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'dct-ccs-finals');
+
+    // Function to connect to the database
+    function db_connection() {
+        $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        // Check for connection errors
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+
+        return $connection;
+    }
+
+    // Usage
+    $connection = db_connection();
+
 ?>
