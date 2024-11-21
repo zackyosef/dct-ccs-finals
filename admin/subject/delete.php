9b598a3d36e6f7e72bcc8bc2a2087ff8213a082e
Lucila Baluyut <lucibaluyutxiii@gmail.com>
2024-11-22 01:30:00 +0800

    fix redirection in delete php in subject

--- admin/subject/delete.php
+++ admin/subject/delete.php
@@ -1,10 +1,9 @@
-<?php guard(); ?>
 <?php
 ob_start(); 
-
 require_once '../../functions.php';
 require_once '../partials/header.php';
 require_once '../partials/side-bar.php';
+guard();
 
 // Initialize variables
 $error_message = '';
