b876f84cbc805bd03481fa5cf280cea5f4fb6f6a
Lucila Baluyut <lucibaluyutxiii@gmail.com>
2024-11-22 10:11:00 +0800

    initialize error/success messages and retrieve record ID from POST or GET

--- admin/student/assign-grade.php
+++ admin/student/assign-grade.php
@@ -3,4 +3,11 @@ ob_start();
 require_once '../partials/header.php';
 require_once '../partials/side-bar.php';
 guard(); 
+
+$error_message = '';
+$success_message = '';
+
+// Get the record ID from either POST or GET
+$record_id = $_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id']) ? intval($_POST['id']) : (isset($_GET['id']) ? intval($_GET['id']) : null);
+
 ?>
